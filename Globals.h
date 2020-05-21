377a0733f011758d65c64e848cd42170385d19bd
KadeCode15 <kadecode15@gmail.com>
2020-05-21 16:35:45 -0500

    Added Files

---
+++ Globals.h
@@ -0,0 +1,6 @@
+float theta3 = -90; //degrees that the 3rd section is off kilter from the horizontal axis. positive is up, negative is down
+
+bool doStopArm = false;
+
+VectorAngles currentAngles = {90, 0, 0, 0, 0};
+Vector currentPoint;
